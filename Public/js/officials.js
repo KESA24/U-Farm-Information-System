1ce32ccea0ddb85ac81fb24ab2d2463ee82f5f95
Kesa <nekepatri@gmail.com>
2020-11-07 16:00:48 +0300

    Added my dependencies for the backend

--- Public/js/officials.js
+++ Public/js/officials.js
@@ -1,10 +1,10 @@
 
+
+// Popup Login Form: Update to latest function writing
 document.getElementById('button').addEventListener('click' ,
 function() {
     document.querySelector('.bg-login').style.display="flex";
-}
-
-)
+})
 
 document.querySelector('.close').addEventListener('click', 
 function(){
